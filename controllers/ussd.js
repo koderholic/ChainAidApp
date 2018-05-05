14385fbac58f7de568ef975294f9137ecea00632
Akindele Akinyemi <akbumper@gmail.com>
2018-05-05 14:37:13 +0100

    fixes

--- controllers/ussd.js
+++ controllers/ussd.js
@@ -4,17 +4,21 @@ const nodemailer = require('nodemailer');
 const User = require('../models/User');
 
 const randomBytesAsync = promisify(crypto.randomBytes);
+const options = {
+    apiKey: '429647716d4133d9f9cdaf5fa0f3ac7248bbfff447dde16443d4c4842f27749a',         // use your sandbox app API key for development in the test environment
+    username: 'sandbox',      // use 'sandbox' for development in the test environment
+  };
+  const AfricasTalking = require('africastalking')(options);
 
-
-exports.postUssd = (req, res, next) => {
+exports.postUssd = new AfricasTalking.USSD((params, next) => {
   let endSession = false;
   let message = '';
   
-  console.log(req.session);
+  //console.log(req.session);
   //const session = req.session.get(params.sessionId);
   //const user = db.getUserByPhone(params.phoneNumber);
 
-  if (req.params.text === '') {
+  if (params.text === '') {
       message = "Welcome to ChainAid \n";
       message += "1: For account info \n";
       message += "2: For lost gas cylinder";
@@ -24,6 +28,6 @@ exports.postUssd = (req, res, next) => {
       response: message, 
       endSession: endSession
   });
-}
+});
 
 
